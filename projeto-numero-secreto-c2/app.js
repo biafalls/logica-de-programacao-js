c36afc35bb7b136c1e441c756cbbb6ef36a44a44
Beatriz Lima <bialima0409@outlook.com>
2026-07-09 16:01:50 -0300

    Aula 3: botão reiniciar funcionando

--- projeto-numero-secreto-c2/app.js
+++ projeto-numero-secreto-c2/app.js
@@ -1,13 +1,16 @@
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+exibirMensagemInicial();
 
 function exibirTextoNaTela (tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial( ) {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -17,6 +20,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você acertou o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if(chute > numeroSecreto){
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -24,6 +28,7 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++;
+        limparCampo();
     }
 }
 
@@ -31,3 +36,15 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10) + 1;
 }
 
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
